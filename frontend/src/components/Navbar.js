0e3e59a1f6a6956f2e223ce3b997bb587aa3cd07
sandralidya10 <lidyasandra2003@gmail.com>
2021-09-23 18:31:18 +0700

    part4

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,11 +2,9 @@ import React from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 
-
 const Navbar = () => {
     return (
         <nav className="navbar">
-
             {/*logo*/}
             <div className="navbar__logo">
                 <h2>K12 Shopping Cart</h2>
@@ -15,10 +13,12 @@ const Navbar = () => {
             {/*links*/}
             <ul className="navbar__links">
                 <li>
-                    <Link to="/cart">
+                    <Link to="/cart" className="cart__link">
                         <i className="fas fa-shopping-cart"></i>
+                        <span>
                         Cart
                         <span className="cartlogo_badge">0</span>
+                        </span>
                     </Link>
                 </li>
                 <li>
