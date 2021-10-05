c423f115ac4a6aacc284102e70929044e944cdaa
sandralidya10 <lidyasandra2003@gmail.com>
2021-10-05 16:42:11 +0700

    part5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 //import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState } from 'react';
 
 //import screens
 import HomeScreen from './screens/HomeScreen';
@@ -9,13 +10,19 @@ import ProductScreen from './screens/ProductScreen';
 
 //import components
 import Navbar from './components/Navbar';
+import Backdrop from './components/Backdrop';
+import SideDrawer from './components/SideDrawer';
 
 function App() {
+
+  const [sideToggle, setSideToggle] = useState(false);
+
+
   return (
     <Router>
-        <Navbar />
-        {/*SideDrawer*/}
-        {/*Backdrop*/}
+      <Navbar click={() => setSideToggle(true)}/>
+      <SideDrawer show={sideToggle} click={() => setSideToggle(false)} />
+        <Backdrop show={sideToggle} click={() => setSideToggle(false)} />
         <main>
           <Switch>
             <Route exact path="/" component={HomeScreen} />
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({click}) => {
     return (
         <nav className="navbar">
             {/*logo*/}
@@ -29,7 +29,7 @@ const Navbar = () => {
             </ul>
 
             {/*hamburger menu*/}
-            <div className="hamburger__menu">
+            <div className="hamburger__menu" onClick={click}>
                 <div></div>
                 <div></div>
                 <div></div>
