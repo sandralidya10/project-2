278cd02befc2e776d22091724c02624cfd60f8a2
sandralidya10 <lidyasandra2003@gmail.com>
2021-10-15 19:35:59 +0700

    part7

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,9 +1,30 @@
 import "./CartScreen.css";
 
+//components
+import CartItem from "../components/CartItem";
+
 const CartScreen = () => {
     return (
-        <div className="cartScreen">
-           Cart Screen
+        <div className="cartscreen">
+            <div className="cartscreen__left">
+                <h2>Your Cart</h2>
+                <CartItem /> 
+                <CartItem />
+                <CartItem />     
+                <CartItem />
+                <CartItem />
+                <CartItem />  
+            </div>
+
+            <div className="cartscreen__right">
+                <div className="cartscreen__info">
+                    <p>Sub Total (0) items</p>
+                    <p>$99.99</p>
+                </div>
+                <div>
+                    <button>Proceed to checkout</button>
+                </div>
+           </div>
         </div>
     )
 }
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -3,7 +3,46 @@ import "./ProductScreen.css";
 const ProductScreen = () => {
     return (
         <div className="productscreen">
-            Product Screen
+            <div className="productScreen__left">
+                <div className="left__image">
+                    <img src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80"
+                    alt="Product Name"
+                    />
+                </div>
+
+                <div className="left__info">
+                    <p className="left__name">Product 1</p>
+                    <p>Price: $99.9</p>
+                    <p>
+                        Description: Welcome to a new era of iPhone. Beautifully bright 6.1-inch Super Retina XDR display.1 Ceramic Shield with 4x better drop performance.2 Incredible low-light photography with Night mode on all cameras. Cinema-grade Dolby Vision video recording, editing, and playback. Powerful A14 Bionic chip. And new MagSafe accessories for easy attach and faster wireless charging.3 Let the fun begin.
+                    </p>
+                </div>
+            </div>
+
+            <div className="productScreen__right">
+            <div className="right__info">
+                <p>
+                    Price: <span>$99.9</span>
+                </p>
+                <p>
+                    Status: <span>In Stock</span>
+                </p>
+                <p>
+                    Qty 
+                    <select>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                    </select>
+                </p>
+                <p>
+                    <button type="button">Add To Cart</button>
+                </p>
+            </div>
+            </div>
         </div>
     )
 }
